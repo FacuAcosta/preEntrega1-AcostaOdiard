3b4774624e47a6f9fe4f261b87e15cb82fedc987
FacuAcosta <facu.acosta14@hotmail.com>
2022-10-31 15:08:27 -0300

    simulador de interes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-nombre (prompt("Ingrese su nombre"))
+let monto = parseInt(prompt("Ingrese monto de la inversion"))
+let interesAnual = parseInt(prompt("Ingrese interes anual de la inversion"))
+let cantidadAnios = parseInt(prompt("Ingrese cantidad de años a invertir"))
+let interesAcumulado = prompt("Quiere que su calculo contenga interes acumulado?\nConteste SI o NO.")
+let montoTotal = monto
+if (interesAcumulado == "SI"){
+for (let i = 0; i < cantidadAnios; i++) {
+    montoTotal = montoTotal + montoTotal * interesAnual / 100
+    console.log("Total en el año ", i + 1, ": ", montoTotal)
+}
+    } else if (interesAcumulado == "NO"){
+        for (let i = 0; i < cantidadAnios; i++) {
+            montoTotal = montoTotal + monto * interesAnual / 100
+            console.log("Total en el año ", i + 1, ": ", montoTotal)
+    }} else console.log("ERORR: INGRESE SI O NO")
